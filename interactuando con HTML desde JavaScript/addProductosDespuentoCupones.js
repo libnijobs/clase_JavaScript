646c7f5b7c5707fe876807fef88618bfb985f4f7
libni <libni.velandia@gmail.com>
2024-01-25 21:47:38 -0400

    acomodando codigo y otro archivo ejemplo de como guardar cupones

--- interactuando con HTML desde JavaScript/addProductosDespuentoCupones.js
+++ interactuando con HTML desde JavaScript/addProductosDespuentoCupones.js
@@ -4,30 +4,41 @@ const inputcupon = document.getElementById("coupon");
 const botonCalcular = document.getElementById("calcular");
 const spam = document.getElementById("total");
 let productosArray = [];
-
 botonCalcular.addEventListener("click", addProductos);
 
+let cuponesObj = {
+  "OFF": 10,
+  "25per": 25,
+  "obj2024": 15,
+}
+
 class Productos {
-  constructor(producto, precio, cupon) {
+  constructor(producto, precio) {
     this.producto = producto;
     this.precio = precio;
-    this.cupon = cupon;
   }
 }
 
 function addProductos() {
-  const nuevoProducto = new Productos(inputproducto.value, inputprecio.value, inputcupon.value);
+  const nuevoProducto = new Productos(inputproducto.value, inputprecio.value);
   productosArray.push(nuevoProducto);
   console.log(productosArray);
 
   mostrarResultados(nuevoProducto);
 }
 
-function calcularPrecioConDescuento(producto, precio, cupon) {
-  return precio - cupon;
+function calcularPrecioConDescuento(precio, cupon) {
+  if (cuponesObj.hasOwnProperty(cupon)) {
+    const descuentoPorcentaje = cuponesObj[cupon];
+    const descuento = precio - (precio * descuentoPorcentaje) / 100;
+    return descuento
+  } else {
+    alert("Cupón no encontrado");
+    return precio; // Devuelve el precio sin descuento si el cupón no es válido
+  }
 }
 
 function mostrarResultados(producto) {
-  const total = calcularPrecioConDescuento(producto.producto, producto.precio, producto.cupon);
+  const total = calcularPrecioConDescuento(producto.precio, inputcupon.value);
   spam.innerHTML = `Último producto agregado: ${producto.producto}, Total: ${total}`;
 }
