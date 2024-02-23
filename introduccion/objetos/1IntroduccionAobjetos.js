73175737a063ee67cfea85f6ffa8d97686890411
libni <libni.velandia@gmail.com>
2024-02-23 06:55:05 -0400

    mas

--- introduccion/objetos/1IntroduccionAobjetos.js
+++ introduccion/objetos/1IntroduccionAobjetos.js
@@ -1,6 +1,13 @@
 let user = {name: "Maria", age: 20,};  // sintaxis de "objeto literal"
 
-user.fruta = "Manzana"
+user.fruta = "Manzana" // podemos agregar propiedades al objeto de esta forma
+user.fruta2 = "uva"
 console.log(user);
+console.log(user.fruta); // podemos buscar una propiedad  del usuario con el punto (.)
+
+// para eliminar una propiedad se utiliza la palabra reservada delete
+delete user.age; // elimina la propiedad edad del usuario
+
+
 
 
